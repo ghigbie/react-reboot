8d5b32020d4788c1fa609c02cce0c6d02a18bbc6
ghigbie <gehigbie@gmail.com>
2019-07-30 21:18:21 -0600

    Add new components

---
+++ react4/YoBaby.js
@@ -0,0 +1,11 @@
+class YoBaby extends React.Component{
+    render(props){
+        return (
+            <div>
+                <h1> Yo {this.props.name} !</h1>
+                <h1> Yo {this.props.name} !</h1>
+                <h1> Yo {this.props.name} !</h1>
+            </div>
+        );
+    }
+}
