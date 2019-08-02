c14bc081476296372d0039874a22d8bb7bb85053
ghigbie <gehigbie@gmail.com>
2019-08-01 20:23:23 -0600

    Import just pokedex

--- pokemon/src/Pokedex.js
+++ pokemon/src/Pokedex.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import Pokecard from './Pokecard';
 import './Pokedex.css';
 
 class Pokedex extends Component{
+    static defaultProps = {
+        pokemon: [
+            {id: 4, name: 'Charmander', type: 'fire', exp: 62},
+            {id: 7, name: 'Squirtle', type: 'wate', exp: 62},
+            {id: 11, name: 'Metapod', type: 'bu', exp: 62},
+            {id: 12, name: 'Butterfree', type: 'flying', exp: 62},
+            {id: 25, name: 'Pikachu', type: 'electric', exp: 62},
+            {id: 39, name: 'Jigglypuff', type: 'normal', exp: 62},
+            {id: 94, name: 'Gengar', type: 'poison', exp: 62},
+            {id: 133, name: 'Eevee', type: 'nomal', exp: 62},
+        ]
+    };
     render(){
+        const pokemons = this.props.pokemon.map((pokemon) => (
+           <Pokecard 
+                id={pokemon.id} 
+                name={pokemon.name} 
+                type={pokemon.type}
+                exp={pokemon.exp}/>
+        ));
         return(
             <div class="Pokedex">
-
+                <h1>Pokedex!</h1>
+                { pokemons }
             </div>
         );
     }
