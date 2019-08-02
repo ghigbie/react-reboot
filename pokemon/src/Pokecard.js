c38c6e95738ba54fb8fcdbbfa4f1fb521e7c7f0f
ghigbie <gehigbie@gmail.com>
2019-08-01 19:48:49 -0600

    Add pokecard component

---
+++ pokemon/src/Pokecard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Pokecard extends Component{
+    render(){
+        return(
+            <div className="pokecard">
+                <h1>Pokecard!</h1>
+            </div>
+        );
+    }
+}
