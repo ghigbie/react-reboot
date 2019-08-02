956f0bf99f03c6f4ea7ef6af4b32a2babdea33f5
ghigbie <gehigbie@gmail.com>
2019-08-01 20:44:42 -0600

    change styles

--- pokemon/src/Pokecard.js
+++ pokemon/src/Pokecard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Pokecard.css'
+import './Pokecard.css';
 
 const POKE_API = 'https://raw.githubbusercontent.com/PokeAPI/spites/master/sprites/pokemon/';
 
@@ -9,12 +9,12 @@ class Pokecard extends Component{
         let imgSrc = `${POKE_API}${props.id}.png`
         return(
             <div className="Pokecard">
-                <h1> {props.name} </h1>
+                <h1 className="Pokecard-title"> {props.name} </h1>
                 <img 
                     src={imgSrc} 
                     alt={props.name}/>
-                <div>Type: {props.type}</div>
-                <div>Exp: {props.exp}</div>
+                <div className="Pokecard-data">Type: {props.type}</div>
+                <div className="Pokecard-data">Exp: {props.exp}</div>
             </div>
         );
     }
