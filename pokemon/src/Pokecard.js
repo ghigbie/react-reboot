f1e7d2db458d19deafd6da334d6b9e971b848e1f
ghigbie <gehigbie@gmail.com>
2019-08-01 21:06:54 -0600

    Add padtothree function

--- pokemon/src/Pokecard.js
+++ pokemon/src/Pokecard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubbusercontent.com/PokeAPI/spites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component{
     render(){
         const props = this.props;
-        let imgSrc = `${POKE_API}${props.id}.png`
+        let imgSrc = `${POKE_API}${padToThree(props.id)}.png`
         return(
             <div className="Pokecard">
                 <h1 className="Pokecard-title"> {props.name} </h1>
