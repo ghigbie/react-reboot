088b817fd5d44cd0640a62eb841c9f02f113a67d
ghigbie <gehigbie@gmail.com>
2019-08-03 13:36:47 -0600

    Add die component

---
+++ roll-dice/src/Die.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component{
+    constructor(props){
+        super(props);
+        state = {
+
+        }
+    }
+    
+    render(){
+        this.props = props;
+        return (
+            <div>
+                <h1>Dice </h1>
+            </div>
+        )
+    }
+}
