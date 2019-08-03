aa16a71958bf8fb5dfbd651629d4efd5a8ebf9c6
ghigbie <gehigbie@gmail.com>
2019-08-03 16:45:10 -0600

    Lottery working

--- lotery/src/App.js
+++ lotery/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lottery  from './Lottery';
 
 function App() {
   return (
     <div className="App">
-
+      <Lottery />
     </div>
   );
 }
---
+++ lotery/src/Lottery.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Ball from './Ball';
+import './Lottery.css';
+
+
+class Lottery extends Component{
+    static defaultProps = {
+        title: 'Loto',
+        maxBalls: 7,
+        maxNumber: 69,
+
+    }    
+
+    constructor(props){
+        super(props);
+        this.state = {
+            numbers: Array.from({ length: this.props.maxBalls }),
+        }
+        this.runLottery = this.runLottery.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    runLottery(){
+        console.log('Run Lottery called');
+        this.setState(curState => ({
+            numbers: curState.numbers.map(
+                num => Math.floor(Math.random() * this.props.maxNumber) + 1)
+        }));
+    }
+
+    handleClick(){
+        this.runLottery();
+    }
+
+    render(){
+        return(
+            <section className="Lottery">
+                <h1>{ this.props.title }</h1>
+                <div className="ball-holder">
+                    {this.state.numbers.map((number) => (<Ball number={number} />))}
+                </div>
+                <button
+                    onClick={this.handleClick}>Run Loto</button>
+            </section>
+        )
+    }
+}
+
+
+export default Lottery;
