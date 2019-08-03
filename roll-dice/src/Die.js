f6df1cf34e43ef4bd07381c85492fc86e3d6aae9
ghigbie <gehigbie@gmail.com>
2019-08-03 13:51:50 -0600

    Add random numbers generators to set state

--- roll-dice/src/Die.js
+++ roll-dice/src/Die.js
@@ -2,19 +2,14 @@ import React, { Component } from 'react';
 import './Die.css';
 
 class Die extends Component{
-    constructor(props){
-        super(props);
-        state = {
-
-        }
-    }
-    
     render(){
-        this.props = props;
+        const props = this.props;
         return (
             <div>
-                <h1>Dice </h1>
+                <h1>Dice {props.number} </h1>
             </div>
         )
     }
 }
+
+export default Die;
---
+++ roll-dice/src/RollDice.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Die from './Die';
+import './Die.css';
+
+class RollDice extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            number1: 1,
+            number2: 2,
+        };
+        this.randomNumber = this.randomNumber.bind(this);
+        this.rollDice = this.rollDice.bind(this);
+    }
+    randomNumber(){
+        return Math.floor(Math.random()*6)+1;
+    }
+    rollDice(){
+        this.setSate({
+            number1: this.randomNumber(),
+            number2: this.randomNumber(),
+        })
+    }
+    render(){
+        return (
+            <div>
+                <div className="dice-container">
+                    <Die number={this.state.number1}/>
+                    <Die number={this.state.number2}/>
+                </div>
+                <br/>
+                <br/>
+                <button
+                    onClick={this.rollDice}>
+                    Roll Dice
+                </button>
+            </div>
+        );
+    }
+}
+
+export default RollDice;
