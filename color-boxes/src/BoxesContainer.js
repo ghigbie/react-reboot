766a19955c376f0cb8b78fc76974518d07acf313
ghigbie <gehigbie@gmail.com>
2019-08-04 12:52:51 -0600

    Add importation of component

---
+++ color-boxes/src/BoxesContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './BoxesContainer';
+
+class BoxesContainer extends Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <div className="BoxesContainer">
+
+            </div>
+        );
+    }
+
+}
+
+export default BoxesContainer;
