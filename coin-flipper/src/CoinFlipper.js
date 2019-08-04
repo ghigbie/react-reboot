7abc23cd912003121f76ce8852fbbdf8a5a8dd88
ghigbie <gehigbie@gmail.com>
2019-08-03 18:30:56 -0600

    Add new component

---
+++ coin-flipper/src/CoinFlipper.js
@@ -0,0 +1,24 @@
+import React , { Component } from  'react';
+import './CoinFlipper.css';
+
+class CoinFlipper extends Component{
+    static defaultProps = {
+
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <section className="CoinFlipper">
+
+            </section>
+        );
+    }
+}
+
+export default CoinFlipper;
