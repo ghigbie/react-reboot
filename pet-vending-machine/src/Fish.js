02379d12842564ee057812d1e1c81a7dce14d8ed
ghigbie <gehigbie@gmail.com>
2019-08-06 20:51:24 -0600

    Add new component

---
+++ pet-vending-machine/src/Fish.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Fish extends Component {
+    render() {
+        <div className="Fish">
+            Bubbles!
+        </div>
+    }
+}
+
+export default Bubbles;
---
+++ pet-vending-machine/src/SmallCat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SmallCat extends Component {
+    render() {
+        <div className="SmallCat">
+            Just joking! I'm a lion. Roar!
+        </div>
+    }
+}
+
+export default SmallCat;
