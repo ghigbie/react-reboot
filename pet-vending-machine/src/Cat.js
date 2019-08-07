3908fd40fd6060953474a85a0f17e9f59915414c
ghigbie <gehigbie@gmail.com>
2019-08-06 20:49:59 -0600

    Add new components

---
+++ pet-vending-machine/src/Cat.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Cat extends Component {
+    render() {
+        <div className="Cat">
+            Meow!
+        </div>
+    }
+}
+
+export default Cat;
---
+++ pet-vending-machine/src/Dog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Dog extends Component {
+    render() {
+        <div className="Dog">
+            Woof!
+        </div>
+    }
+}
+
+export default Dog;
