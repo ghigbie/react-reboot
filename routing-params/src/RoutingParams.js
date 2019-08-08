655ecaeb7322d00c855cd281ffcfea1351663616
ghigbie <gehigbie@gmail.com>
2019-08-07 19:58:26 -0600

    Add new component

---
+++ routing-params/src/RoutingParams.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './RoutingParams.css'
+
+class RoutingParams extends Component{
+    render(){
+        return(
+            <div className="RoutingParams">
+
+            </div>
+        );
+    }
+}
