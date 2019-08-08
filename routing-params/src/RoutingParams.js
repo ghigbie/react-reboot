8b23e86d0ed6158c007dc41d672cfda776806736
ghigbie <gehigbie@gmail.com>
2019-08-07 20:14:14 -0600

    Add new component

--- routing-params/src/RoutingParams.js
+++ routing-params/src/RoutingParams.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
-import './RoutingParams.css'
+import { NavLink, Route, Switch } from 'react-router-dom';
+ import './RoutingParams.css'
 
 class RoutingParams extends Component{
     render(){
         return(
             <div className="RoutingParams">
 
+                <Switch>
+                    <Route 
+                        exact
+                        path='/food/:name/'
+                        render={routeProps => <Food {...routeProps} />}
+                    />
+                </Switch>
             </div>
         );
     }
