b53a84cd425a77885203771a650dea62ac6ff648
ghigbie <gehigbie@gmail.com>
2019-08-09 20:47:59 -0600

    Add new project

--- context-app-practice/src/components/Navbar/Navbar.jsx
+++ context-app-practice/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Navbar from './Navbar'
 
 class Navbar extends Component{
     render(){
         return(
             <div class="Navbar">
-                Navbar! Yay!
+                <h1>Navbar! Yay!</h1>
             </div>
         );
     }
