6b20847a0cc41abf43b6ebd0f2a5e5bbca40778d
ghigbie <gehigbie@gmail.com>
2019-08-09 21:57:57 -0600

    Add new route

--- dog-router/src/components/dog-app/dog-app.component.jsx
+++ dog-router/src/components/dog-app/dog-app.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DogList from './../dog-list/dog-list.component';
+import DogDetails from './../dog-details/dog-details.component';
 import {Switch, Route} from 'react-router-dom';
 import whiskey from "./../images/whiskey.jpg";
 import tubby from "./../images/tubby.jpg";
@@ -42,14 +43,27 @@ class DogApp extends Component {
         ]
     };
     render() {
+        const getDog = props => {
+            let name = props.match.params.name;
+            let currentDog = this.props.dogs.find(
+                dog =>dog.name.toLowerCase() === name.toLowerCase());
+            return <DogDetails {...props} dog={currentDog} />;
+        }
         return (
             <div className="DogApp">
                 DoggieApp!!!
-
-                <Route 
-                    path="/dogs" 
-                    render={() => <DogList dogs={this.props.dogs}/>}
-                />
+                <Switch>
+                    <Route 
+                        exact
+                        path="/dogs" 
+                        render={() => <DogList dogs={this.props.dogs}/>}
+                    />
+                    <Route
+                        exact
+                        path="/dog/:name"
+                        render={getDog}
+                    />
+                </Switch>
             </div>
         );
     }
--- dog-router/src/components/dog-list/dog-list.component.jsx
+++ dog-router/src/components/dog-list/dog-list.component.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
-import './dog-list.component';
+import './dog-list.styles.css';
 
 class DogList extends Component{
     render(){
         return(
             <div className="DogList">
-                {this.props.dogs}
+                <h1 className="display-1 text-center">Doggie List</h1>
+                    <div className="container">
+                        <div className='row'>
+                        {this.props.dogs.map((dog) => (
+                            <div className='Dog col-md-6 col-lg-4 text-center'>
+                                <img src={dog.src}
+                                     alt={dog.name} />
+                                <h3>{dog.name}</h3>
+                            </div>
+                        ))}
+                        </div>
+                    </div>
             </div>
         );
     }
