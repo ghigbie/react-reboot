1340de97634c7a2b1d7988509ca1345394f9f814
ghigbie <gehigbie@gmail.com>
2019-08-09 21:23:05 -0600

    Add browser router

--- dog-router/src/App.js
+++ dog-router/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import DogApp from './components/DogApp/DogApp';
+import DogApp from './components/dog-app/dog-app.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <DogApp />
+      <h1 className="display-1">Moo</h1>
     </div>
   );
 }
--- dog-router/src/components/dog-app/dog-app.component.jsx
+++ dog-router/src/components/dog-app/dog-app.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './DogApp.css';
+import './dog-app.styles.css';
 
 class DogApp extends Component {
     static defaultProps = {
