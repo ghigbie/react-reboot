6ca23b18ac9014fcd7a3dcafde30bbaad919c2ba
ghigbie <gehigbie@gmail.com>
2019-08-10 19:15:21 -0600

    Style up color box

--- color-pallet-app/src/components/Palette.jsx
+++ color-pallet-app/src/components/Palette.jsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
-import ColorBox from './components/ColorBox';
+import ColorBox from './ColorBox';
+import './Palette.css';
 
 class Palette extends Component{
     render(){
+        const colorBoxes = this.props.colors.map((color, index) => (
+            <ColorBox 
+                background={color.color} 
+                name={color.name} 
+                key={index}/>
+        ));
         return(
             <div className="Palette">
                 {/* Navbar goes here */}
                 <div className="Palette-colors">
-                {/* foor eventually */}
+                { colorBoxes }
+                {/* footer eventually */}
                 </div>
             </div>
         )
