4db74f4d072ec6365ebe9d0de81028d30ceb306b
ghigbie <gehigbie@gmail.com>
2019-08-10 19:04:20 -0600

    Add JSX for colorbox

--- color-pallet-app/src/App.js
+++ color-pallet-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Pallete from './components/Palette';
+import Palette from './components/Palette';
 import seedColors from './utilities/seedColors';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Palette />
+        <Palette {...seedColors[4]}/>
     </div>
   );
 }
--- color-pallet-app/src/components/Palette.jsx
+++ color-pallet-app/src/components/Palette.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import ColorBox from './components/ColorBox';
 
 class Palette extends Component{
     render(){
         return(
             <div className="Palette">
-
+                {/* Navbar goes here */}
+                <div className="Palette-colors">
+                {/* foor eventually */}
+                </div>
             </div>
         )
     }
