1f3a83e0f623259c62f4386f670c2c55f949b412
ghigbie <gehigbie@gmail.com>
2019-08-10 19:59:38 -0600

    Add change copy state function

--- color-pallet-app/src/components/ColorBox.jsx
+++ color-pallet-app/src/components/ColorBox.jsx
@@ -5,14 +5,25 @@ import './ColorBox.css';
 class ColorBox extends Component {
     constructor(props){
         super(props);
-        this.state = { copied: false };
+        this.state = { 
+            copied: false 
+        };
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+    changeCopyState(){
+        this.setState({copied: true}, () => {
+            setTimeout(() => this.setState({ copied: false}), 1500);
+        });
     }
     render() {
         const { name, background } = this.props
         return (
-            <CopyToClipboard text={background}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{backgroundColor: background }}
                     className="ColorBox">
+                    <div style={{ backgroundColor: background }}
+                         className="copy-overlay">
+                    </div>
                     <div className='copy-container'>
                         <div className="box-content">
                             <span>{name}</span>  
