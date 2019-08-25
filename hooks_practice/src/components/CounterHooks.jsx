01fe83fcf66a0dad0dad76ce3df81cf0f75ff6ed
ghigbie <gehigbie@gmail.com>
2019-08-25 13:55:32 -0600

    Remove boilerplate code

---
+++ hooks_practice/src/components/CounterHooks.jsx
@@ -0,0 +1,11 @@
+import React, { useState }from 'react'
+
+function CounterHooks() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CounterHooks
---
+++ hooks_practice/src/components/SimpleFormHooks.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const SimpleFormHooks = () => {
+        return (
+            <div>
+                
+            </div>
+        );
+}
