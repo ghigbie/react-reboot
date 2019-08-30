a42c4876c2e6768c148505adc6ce0eefa3f1c188
ghigbie <gehigbie@gmail.com>
2019-08-30 11:13:46 -0500

    Remove errors

--- todo-list/src/components/todo_app/TodoApp.jsx
+++ todo-list/src/components/todo_app/TodoApp.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from'@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 
-import TodoList from './todo_list/TodoList';
+import TodoList from './../todo_list/TodoList';
 
 
 const TodoApp = () => {
