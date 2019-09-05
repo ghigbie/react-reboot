754f873c022485b1f86e9d14ebbacc2bf9155be9
ghigbie <gehigbie@gmail.com>
2019-09-04 20:52:11 -0600

    Fix import bugs

--- todo-list/src/components/todo/todo.jsx
+++ todo-list/src/components/todo/todo.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
-const todo = () => {
+const todo = ({ task, completed }) => {
     return (
-        <div>
-            
-        </div>
+        <ListItem>
+            <ListItemText>
+                {task}
+            </ListItemText>
+        </ListItem>
     )
 }
+
+export default todo;
--- todo-list/src/components/todo_list/TodoList.jsx
+++ todo-list/src/components/todo_list/TodoList.jsx
@@ -2,20 +2,17 @@ import React, {useState, Fragment} from 'react'
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText  from '@material-ui/core/ListItemText';
+
+import Todo from './../todo/todo';
 
 const TodoList = (props) => {
     return (
     <Paper>
         <List>
             {props.todos.map( todo => (
-                <Fragment>
-                    <ListItem>
-                        <ListItemText>
-                            {todo.task}
-                        </ListItemText>
-                    </ListItem>
+                <Fragment key={todo.id}>
+                    <Todo task={todo.task}
+                          completed={todo.completed} />
                     <Divider />
                 </Fragment>
             ))}
