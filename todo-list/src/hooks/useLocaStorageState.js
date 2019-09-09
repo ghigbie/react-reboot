491510a55604c6cdb6e0d1ae57aca6c09b064126
ghigbie <gehigbie@gmail.com>
2019-09-08 19:50:09 -0600

    Add js object and function

---
+++ todo-list/src/hooks/useLocaStorageState.js
@@ -0,0 +1,7 @@
+import {useState, useEffect} from 'react';
+
+const UseLocalStorageState = (key, defaultValue) => {
+    const [state, setState] = useState();
+}
+
+export default UseLocalStorageState;
