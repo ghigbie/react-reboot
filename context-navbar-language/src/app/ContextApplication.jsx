4030fe20eed881a24759c6aa25835807121d3421
ghigbie <gehigbie@gmail.com>
2019-09-09 21:51:33 -0600

    Add responsive navbar

--- context-navbar-language/src/app/ContextApplication.jsx
+++ context-navbar-language/src/app/ContextApplication.jsx
@@ -1,13 +1,4 @@
 import React, { Component } from 'react';
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import Inputbase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/icons/Search";
-import Switch from "@material-ui/core/Switch";
-import { withStyles } from "@material-ui/core/styles";
-
 import Navbar from './../components/navbar/Navbar';
 
 class ContextApplication extends Component {
