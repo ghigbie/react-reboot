40399d58e464b4d3a65c11d321165f87420b0823
ghigbie <gehigbie@gmail.com>
2019-09-09 20:46:47 -0600

    Import navbar

---
+++ context-navbar-language/src/app/ContextApplication.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Navbar from './components/navbar/Navabar';
+
+class ContextApplication extends Component {
+    render() {
+        return (
+            <div className="context-application">
+                <Navbar />
+            </div>
+        )
+    }
+}
+
+export default ContextApplication;
