17af23658a10fe4fb7dd44d4b9f093ac485ea1b4
ghigbie <gehigbie@gmail.com>
2019-09-11 21:29:45 -0600

    Edit styles

--- ecom-shop/src/components/directory/directory.component.jsx
+++ ecom-shop/src/components/directory/directory.component.jsx
@@ -46,7 +46,7 @@ class Directory extends Component{
 
     render(){
         return (
-            <div className="directory">
+            <div className="directory-menu">
                 {this.state.sections.map(({ title, id, imageUrl, size}) => (
                     <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                 ))}
--- ecom-shop/src/components/menu-item/menu-item.component.jsx
+++ ecom-shop/src/components/menu-item/menu-item.component.jsx
@@ -4,10 +4,10 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size}) => (
     <div 
-        className={`${size}menu-item`}
+        className={`${size} menu-item`}
         style={{ backgroundImage: `url(${imageUrl})`}}>
         <div className="content">
-            <h1 className="title">{ title.toUppercase() }</h1>
+            <h1 className="title">{ title.toUppercase }</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
