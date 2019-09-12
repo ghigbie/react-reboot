a2685be52ee04ba38baba81f056de97353d03d9e
ghigbie <gehigbie@gmail.com>
2019-09-11 21:22:08 -0600

    Remove logo import

--- ecom-shop/src/components/menu-item/menu-item.component.jsx
+++ ecom-shop/src/components/menu-item/menu-item.component.jsx
@@ -7,7 +7,7 @@ const MenuItem = ({ title, imageUrl, size}) => (
         className={`${size}menu-item`}
         style={{ backgroundImage: `url(${imageUrl})`}}>
         <div className="content">
-            <h1 className="title">{ title }</h1>
+            <h1 className="title">{ title.toUppercase() }</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
