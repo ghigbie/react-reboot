1ebfbd75673781f507341e9d04e335aa7deb8e74
ghigbie <gehigbie@gmail.com>
2019-09-11 20:10:15 -0600

    Add new compoents

---
+++ ecom-shop/src/components/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-mnue">
+            <div className="menuitem">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="title"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
