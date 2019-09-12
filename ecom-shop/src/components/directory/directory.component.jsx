91b0535fb485bcd8aa12d0e3743e180a95eb2253
ghigbie <gehigbie@gmail.com>
2019-09-11 20:48:46 -0600

    Add new class base directory

---
+++ ecom-shop/src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './directory.styles.scss';
+
+class Directory extends Component{
+    render(){
+        return (
+            <div className="directory">
+
+            </div>
+        );
+    }
+}
