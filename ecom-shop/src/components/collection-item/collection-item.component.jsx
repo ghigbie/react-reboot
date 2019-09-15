f3abc8ebcae46b282982148636035c7474f3d677
ghigbie <gehigbie@gmail.com>
2019-09-15 14:15:21 -0600

    Add styles for files

--- ecom-shop/src/components/collection-item/collection-item.component.jsx
+++ ecom-shop/src/components/collection-item/collection-item.component.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = () => (
+const CollectionItem = ({id, name, price, imageUrl}) => (
     <div class="collection-item">
+        <div className="image"
+             style={{ backgroundIamge: `url(${imageUrl})`}}
+        </div>
     </div>
 )
