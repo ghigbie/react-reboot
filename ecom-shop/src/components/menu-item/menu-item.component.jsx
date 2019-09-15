9db75260659bbab4f765fab3101dbedc913355be
ghigbie <gehigbie@gmail.com>
2019-09-14 21:41:45 -0600

    Add basic navigation

--- ecom-shop/src/components/menu-item/menu-item.component.jsx
+++ ecom-shop/src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,7 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match}) => (
     <div className={`${size} menu-item`}
-         onCick={() => history.push()}>
+         onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image'
              style={{ backgroundImage: `url(${imageUrl})`}} />
         <div className="content">
