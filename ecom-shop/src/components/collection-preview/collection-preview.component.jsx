b9c31003b78ad4ae1c21c735ade970700d64b1fc
ghigbie <gehigbie@gmail.com>
2019-09-14 21:51:53 -0600

    Add collection preview component

---
+++ ecom-shop/src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './collection-preview.styles.csss';
+
+const CollectionPreview = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CollectionPreview;
