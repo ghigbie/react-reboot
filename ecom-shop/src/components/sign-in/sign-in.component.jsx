60727f343c5755e77c05fe79cae6c26347f44008
ghigbie <gehigbie@gmail.com>
2019-09-15 20:15:59 -0600

    Add labels

--- ecom-shop/src/components/sign-in/sign-in.component.jsx
+++ ecom-shop/src/components/sign-in/sign-in.component.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
+import './sign-in.styles.scss';
+
 class SignIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
     render() {
         return (
             <div className="sign-in">
-                
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form>
+                    <input name="email" type="email" value={this.state.email} required/>
+                    <label htmlFor="email">Email</label>
+                    <input name="password" type="password" value={this.state.password} required/>
+                    <label htmlFor="password">Password</label>
+                    <input type="submit" value='Submit Form'/>
+                </form>
             </div>
         )
     }
