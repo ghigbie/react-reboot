870c19d6ef6557c21da15a5ad0bbbcd8e7687301
ghigbie <gehigbie@gmail.com>
2019-09-15 20:07:00 -0600

    Import component

--- ecom-shop/src/App.js
+++ ecom-shop/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignOut from './pages/sign-in-and-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/signin' component={SignInAndSignOut} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
