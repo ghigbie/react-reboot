e5ce9da62328a34bd90c68271df3c6ced2507715
ghigbie <gehigbie@gmail.com>
2019-09-18 18:27:50 -0600

    Edit firebase

--- ecom-shop/src/components/sign-in/sign-in.component.jsx
+++ ecom-shop/src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,8 @@ import './sign-in.styles.scss';
 import FormInput from './../form-input/form-input.component';
 import CustomButton from './../custom-button/custom-button.component';
 
+import { signInWithGoogle } from './../../firebase/firebase.utils';
+
 class SignIn extends Component {
     constructor(props){
         super(props);
@@ -48,6 +50,7 @@ class SignIn extends Component {
                         label="password"
                         required/>
                     <CustomButton type="submit">Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
                 </form>
             </div>
         )
--- ecom-shop/src/firebase/firebase.utils.js
+++ ecom-shop/src/firebase/firebase.utils.js
@@ -1,3 +1,24 @@
 import firebase from 'firebase/app';
-import 'firebase.firestore';
+import 'firebase/firestore';
 import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB2yB8Ba-rHBOHrBid_0p5HJzfEj12OZx4",
+    authDomain: "ecom-shop-6d712.firebaseapp.com",
+    databaseURL: "https://ecom-shop-6d712.firebaseio.com",
+    projectId: "ecom-shop-6d712",
+    storageBucket: "",
+    messagingSenderId: "354925328796",
+    appId: "1:354925328796:web:bb60d03070cc6f8221b059"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
